0961294ecd4ad6f4ea71ce63461f2df8e27f0a29
Cole Glotfelty <16599506+cole-glotfelty@users.noreply.github.com>
2025-11-16 12:35:17 +0000

    Remove devenv disable since module was removed entirely

--- hosts/alpha-1-5/users/coleglotfelty.nix
+++ hosts/alpha-1-5/users/coleglotfelty.nix
@@ -3,9 +3,6 @@
 {
   # Host-specific overrides for alpha-1-5 - template imports are handled automatically
   
-  # Temporarily disable devenv due to cachix build issues on macOS
-  features.cli.devenv.enable = false;
-  
   # Temporarily disable kitty due to fish build issues on macOS
   features.applications.kitty.enable = false;
 }
