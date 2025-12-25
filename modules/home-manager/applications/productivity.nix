90cdac2ad368c35b0eb4c14a68d88d6897c9a749
Cole Glotfelty <git@postagepaid.cc>
2025-12-25 17:47:08 -0500

    Added Libre Office

--- modules/home-manager/applications/productivity.nix
+++ modules/home-manager/applications/productivity.nix
@@ -8,6 +8,15 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       obsidian
+      libreoffice
     ];
+
+    # LibreOffice: Configure manually for dark UI with white document background
+    # 1. Open LibreOffice
+    # 2. Go to Tools > Options > LibreOffice > Application Colors
+    # 3. Uncheck "Use automatic color for Document background" checkbox
+    # 4. Set "Document background" color to white
+    # 5. Click OK
+    # The UI will use your dark theme but documents will have white backgrounds
   };
 }
